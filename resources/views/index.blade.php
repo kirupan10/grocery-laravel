e4bd1796ae8a037f05c311b4a8ec1fdffced9579
kirupan10 <ikirupan@gmail.com>
2023-02-03 10:28:21 +0530

    login form UI added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -61,7 +61,7 @@
             <div class="col-lg-3 d-none d-lg-block">
                 <a href="" class="text-decoration-none">
                     <h1 class="m-0 display-5 font-weight-semi-bold"><span
-                            class="text-primary font-weight-bold border px-3 mr-1">E</span>Shopper</h1>
+                            class="text-primary font-weight-bold border px-3 mr-1">E</span>Grocery</h1>
                 </a>
             </div>
             <div class="col-lg-6 col-6 text-left">
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,24 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
+
+
+Route::get('/shop', function () {
+    return view('shop');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
 
