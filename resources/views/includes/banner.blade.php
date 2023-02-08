83d67fd5cdb38d6d496d06361a78c10badfab6a2
kirupan10 <ikirupan@gmail.com>
2023-02-08 22:33:49 +0530

    Homepage banner patricianly separated
    Banner patricianly separated and @include using so we can easily change banners.

---
+++ resources/views/includes/banner.blade.php
@@ -0,0 +1,87 @@
+<div id="header-carousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+        <!--- Banner Start --->
+        <div class="carousel-item" style="height: 410px;">
+            <img class="img-fluid" src="img/carousel-2.jpg" alt="Image">
+            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                <div class="p-3" style="max-width: 700px;">
+                    <h4 class="text-light text-uppercase font-weight-medium mb-3">Latest iPhone</h4>
+                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">Just 526,000.00</h3>
+                    <a href="/shop" class="btn btn-light py-2 px-3">Shop Now</a>
+                </div>
+            </div>
+        </div>
+        <!--- Banner End --->
+        <!--- Banner Start --->
+        <div class="carousel-item" style="height: 410px;">
+            <img class="img-fluid" src="img/carousel-5.jpg" alt="Image">
+            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                <div class="p-3" style="max-width: 700px;">
+                    <h4 class="text-light text-uppercase font-weight-medium mb-3">Above purchasing Rs 4500.00</h4>
+                    <h3 class="display-4 text-white font-weight-semi-bold mb-4"> Save Rs 450.00</h3>
+                    <a href="/shop" class="btn btn-light py-2 px-3">Shop Now</a>
+                </div>
+            </div>
+        </div>
+        <!--- Banner End --->
+        <!--- Banner Start --->
+        <div class="carousel-item" style="height: 410px;">
+            <img class="img-fluid" src="img/carousel-3.jpg" alt="Image">
+            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                <div class="p-3" style="max-width: 700px;">
+                    <h4 class="text-light text-uppercase font-weight-medium mb-3">from Rs 49,990.00</h4>
+                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">Gift for Every Budget</h3>
+                    <a href="/shop" class="btn btn-light py-2 px-3">Shop Now</a>
+                </div>
+            </div>
+        </div>
+        <!--- Banner End --->
+        <!--- Banner Start --->
+        <div class="carousel-item" style="height: 410px;">
+            <img class="img-fluid" src="img/carousel-1.jpg" alt="Image">
+            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                <div class="p-3" style="max-width: 700px;">
+                    <h4 class="text-light text-uppercase font-weight-medium mb-3">10% Off Your First
+                        Order</h4>
+                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">Reasonable Price</h3>
+                    <a href="/shop" class="btn btn-light py-2 px-3">Shop Now</a>
+                </div>
+            </div>
+        </div>
+        <!--- Banner End --->
+        <!--- Banner Start --->
+        <div class="carousel-item active" style="height: 410px;">
+            <img class="img-fluid" src="img/carousel-2.jpg" alt="Image">
+            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                <div class="p-3" style="max-width: 700px;">
+                    <h4 class="text-light text-uppercase font-weight-medium mb-3">40% Off Your Order</h4>
+                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">Unforgettable Impression</h3>
+                    <a href="/shop" class="btn btn-light py-2 px-3">Shop Now</a>
+                </div>
+            </div>
+        </div>
+        <!--- Banner End --->
+        <!--- Banner Start --->
+        <div class="carousel-item" style="height: 410px;">
+            <img class="img-fluid" src="img/carousel-6.jpg" alt="Image">
+            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                <div class="p-3" style="max-width: 700px;">
+                    <h4 class="text-light text-uppercase font-weight-medium mb-3">40% Off Your Order</h4>
+                    <h3 class="display-4 text-white font-weight-semi-bold mb-4">Unforgettable Impression</h3>
+                    <a href="/shop" class="btn btn-light py-2 px-3">Shop Now</a>
+                </div>
+            </div>
+        </div>
+        <!--- Banner End --->
+    </div>
+    <a class="carousel-control-prev" href="#header-carousel" data-slide="prev">
+        <div class="btn btn-dark" style="width: 45px; height: 45px;">
+            <span class="carousel-control-prev-icon mb-n2"></span>
+        </div>
+    </a>
+    <a class="carousel-control-next" href="#header-carousel" data-slide="next">
+        <div class="btn btn-dark" style="width: 45px; height: 45px;">
+            <span class="carousel-control-next-icon mb-n2"></span>
+        </div>
+    </a>
+</div>
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>E-Grocery {{ $title }}</title>
+    <title>Grocery {{ $title }}</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="Free HTML Templates" name="keywords">
     <meta content="Free HTML Templates" name="description">
